4e871630b487845f3793ccf45716be58456c250f
javimatz <jvdelso@gmail.com>
2020-04-16 17:50:55 +0200

    add: JSON web token

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 
@@ -48,7 +49,22 @@ router.post('/', [
 
 			await user.save();
 
-			res.send('User saved');
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload, 
+				config.get('jwtSecret'), {
+					expiresIn: 360000
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 
 		} catch (err) {
 			console.error(err.message);

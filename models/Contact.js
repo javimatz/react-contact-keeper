e5b0a3407cfd3bd5f69197d0518b69e0e2bc466a
javimatz <jvdelso@gmail.com>
2020-04-17 17:45:15 +0200

    add: add new contact route

--- models/Contact.js
+++ models/Contact.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ContactSchema = mongoose.Schema({
 	user: {
-		type: mongoose.Schema.Types.ObjetcId,
+		type: mongoose.Schema.Types.ObjectId,
 		ref: 'users'
 	},
 	name: {

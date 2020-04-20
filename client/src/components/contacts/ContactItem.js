992aca409d3cd90a2e874d19032734191efc863b
javimatz <jvdelso@gmail.com>
2020-04-20 13:30:21 +0200

    add: PropTypes

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ContactItem = ({contact}) => {
 
@@ -26,6 +27,10 @@ const ContactItem = ({contact}) => {
 			</div>
 		</div>
 	)
+}
+
+ContactItem.propTypes = {
+	contact: PropTypes.object.isRequired
 }
 
 export default ContactItem;

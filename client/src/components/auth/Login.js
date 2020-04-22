a2a7ce498832cd017b244680a3af1018c16d7ca3
javimatz <jvdelso@gmail.com>
2020-04-22 13:24:10 +0200

    fix: onSubmit function

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -11,19 +11,15 @@ const Login = () => {
 
 	const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
-	const onSubmit = () => {
+	const onSubmit = (e) => {
 		e.preventDefault();
 		console.log('Login Submit');
 	}
 
 	return (
 		<div class='form-container'>
-			<h1>Account <span className='text-primary'>Register</span></h1>
+			<h1>Account <span className='text-primary'>Login</span></h1>
 			<form onSubmit={onSubmit}>
-				<div className='form-group'>
-					<label htmlFor='name'>Name</label>
-					<input type='text' name='name' value={name} onChange={onChange} />
-				</div>
 				<div className='form-group'>
 					<label htmlFor='email'>Email</label>
 					<input type='email' name='email' value={email} onChange={onChange} />
@@ -32,11 +28,7 @@ const Login = () => {
 					<label htmlFor='password'>Password</label>
 					<input type='password' name='password' value={password} onChange={onChange} />
 				</div>
-				<div className='form-group'>
-					<label htmlFor='password2'>Confirm Password</label>
-					<input type='password' name='password2' value={password2} onChange={onChange} />
-				</div>
-				<input type='submit' value='Register' className='btn btn-primary btn-block' /> 
+				<input type='submit' value='Login' className='btn btn-primary btn-block' /> 
 			</form>
 		</div>
 	)

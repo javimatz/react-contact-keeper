b5906a90a7f54cac31d8dcb447b7dbb5a4a441cc
javimatz <jvdelso@gmail.com>
2020-04-22 13:26:24 +0200

    add: onSubmit function

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -13,10 +13,15 @@ const Register = () => {
 
 	const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
+	const onSubmit = () => {
+		e.preventDefault();
+		console.log('Register Submit');
+	}
+
 	return (
 		<div class='form-container'>
 			<h1>Account <span className='text-primary'>Register</span></h1>
-			<form>
+			<form onSubmit={onSubmit}>
 				<div className='form-group'>
 					<label htmlFor='name'>Name</label>
 					<input type='text' name='name' value={name} onChange={onChange} />

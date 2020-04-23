b5bc5ee77f63d1ff57ae72b50ba12e9c14f6a784
javimatz <jvdelso@gmail.com>
2020-04-23 11:32:45 +0200

    fix: uuid import

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
 import { v4 as uuidv4 } from 'uuid'; 
-import AlertAuthContext from './alertContext';
+import AlertContext from './alertContext';
 import AlertReducer from './alertReducer';
 import { 
 	SET_ALERT, REMOVE_ALERT 
@@ -14,19 +14,20 @@ const AlertState = (props) => {
 
 	// Set Alert
 	const setAlert = (msg, type, timeout = 5000 ) => {
-		const id = uuid.v4();
+		const id = uuidv4();
 		dispatch({
 			type: SET_ALERT,
 			payload: { msg, type, id }
 		});
 
-		setTimeout( () => dispatch({ type: REMOVE_ALERT, payload: id }), timeout):	
+		setTimeout( () => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 	}
 
 	return (
 		<AlertContext.Provider
 			value = {{
-				alerts: state				
+				alerts: state,
+				setAlert			
 		}}>
 		{ props.children }
 		</AlertContext.Provider>

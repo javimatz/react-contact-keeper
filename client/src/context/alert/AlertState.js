0fd2e05770f94c79a2004a8ad606b44fbbdcc5fa
javimatz <jvdelso@gmail.com>
2020-04-23 11:24:43 +0200

    add: alert context files

---
+++ client/src/context/alert/AlertState.js
@@ -0,0 +1,36 @@
+import React, { useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid'; 
+import AlertAuthContext from './alertContext';
+import AlertReducer from './alertReducer';
+import { 
+	SET_ALERT, REMOVE_ALERT 
+} from '../types';
+
+const AlertState = (props) => {
+	
+	const initialState = [];
+
+	const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+	// Set Alert
+	const setAlert = (msg, type, timeout = 5000 ) => {
+		const id = uuid.v4();
+		dispatch({
+			type: SET_ALERT,
+			payload: { msg, type, id }
+		});
+
+		setTimeout( () => dispatch({ type: REMOVE_ALERT, payload: id }), timeout):	
+	}
+
+	return (
+		<AlertContext.Provider
+			value = {{
+				alerts: state				
+		}}>
+		{ props.children }
+		</AlertContext.Provider>
+	);
+};
+
+export default AlertState;

4b4f473d75bd2f867f1ead9d6867632065a769ad
javimatz <jvdelso@gmail.com>
2020-04-23 14:00:13 +0200

    add: import authContext

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,15 @@
 import React, { useState, useContext } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
 	
 	const alertContext = useContext(AlertContext);
+	const authContext = useContext(AuthContext);
 
 	const { setAlert } = alertContext;	
+	
+	const { register } = authContext;	
 	
 	const [user, setUser] = useState({
 		name: '',
